7fc4190cf779dd9bae5370336d132a3b82538ddb
lukevaka <lukevakasisikakala@gmail.com>
2020-05-09 20:52:07 -0400

    Updates

---
+++ precache-manifest.81ac1efe954a18b30a4df91906fe5960.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fdd6e2e5fcdb9b5927507bf5117f691",
+    "url": "/diana.io/index.html"
+  },
+  {
+    "revision": "9596f9c5277a00f4dc93",
+    "url": "/diana.io/static/css/main.81ca5868.chunk.css"
+  },
+  {
+    "revision": "fc546044959ce78286d4",
+    "url": "/diana.io/static/js/2.2aaf8d5b.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/diana.io/static/js/2.2aaf8d5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9596f9c5277a00f4dc93",
+    "url": "/diana.io/static/js/main.3ce20027.chunk.js"
+  },
+  {
+    "revision": "6ec8ee1e662a78ca7ac7",
+    "url": "/diana.io/static/js/runtime-main.3c41de84.js"
+  },
+  {
+    "revision": "be54de9faa837e6b11669ba4ed9ab143",
+    "url": "/diana.io/static/media/Diana.be54de9f.png"
+  },
+  {
+    "revision": "569e969db889453b63cc0f3cc25118b1",
+    "url": "/diana.io/static/media/Diana1.569e969d.png"
+  },
+  {
+    "revision": "b3c5ded49b9d25aa7f1f785ac7ec5209",
+    "url": "/diana.io/static/media/FinalVideo.b3c5ded4.m4v"
+  },
+  {
+    "revision": "3742adec8bad74624fae78bc156674b7",
+    "url": "/diana.io/static/media/Luke.3742adec.png"
+  },
+  {
+    "revision": "3312aaa901bf7923a6f51db0a8cca307",
+    "url": "/diana.io/static/media/Luke1.3312aaa9.png"
+  },
+  {
+    "revision": "5e49542b239b13a3e6e03a7f6a1249d1",
+    "url": "/diana.io/static/media/TakeAChance.5e49542b.mp3"
+  },
+  {
+    "revision": "2c530eae7295a48b8f26cbb1a827ff66",
+    "url": "/diana.io/static/media/cony.2c530eae.png"
+  },
+  {
+    "revision": "6488b969fd86854fad18bfb4a36b05da",
+    "url": "/diana.io/static/media/cony1.6488b969.png"
+  }
+]);
